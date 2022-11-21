3161dfeeb34c2d5175308d676d07df10356241fc
Dariusz Golunski <darek.golunski@gmail.com>
2022-11-21 11:56:29 +0100

    First release

---
+++ src/components/Billing.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style';
+
+const Billing = () => {
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className='w-[100%] h-[100%] z-[5]'/>
+        {/* <div className='absolute z-[1] sm:-left-1 -left-1/4 top-0 w-[50%] h-[50%] rounded-full white__gradient'/>
+        <div className='absolute z-[0] sm:-left-1 -left-1/4 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'/> */}
+        <div className='absolute z-[0] w-[40%] h-[35%] -left-1/2 pink__gradient'/>
+      <div className='absolute z-[1] w-[80%] h-[80%] rounded-full -left-1/2 white__gradient'/>
+      <div className='absolute z-[0] w-[50%] h-[55%] -left-1/2 bottom-20 blue__gradient'/>
+      <div className='absolute z-[0] w-[40%] h-[35%] -left-1/2 pink__gradient'/>
+      </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Easily control your<br className='sm:block hidden'/> billing & invoicing.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
+      <div className='flex flex-row flex-wrap sm:mt-10 mt-6 '>
+      <img  src={apple} alt="apple-store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'/>
+      <img  src={google} alt="google-play" className='w-[128px] h-[42px] object-contain  cursor-pointer'/>
+
+      </div>
+      
+      </div>
+      
+    </section>
+  )
+};
+
+export default Billing;
